8196223923fc97fcd3209b5d0478afe5469c1c27
umiadore <103014269+umiadore@users.noreply.github.com>
2024-06-08 16:49:19 +0400

    added new files

---
+++ src/Example01.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/*
+Подряд вводятся оценк студентов по 100балльной системе
+Количество оценок не ограничено, но когда введется значени
+найдите самую низкую оценку
+ */
+public class Example01 {
+    public static void main(String[] args) {
+        List<Integer> marks = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+            int value = scanner.nextInt();
+            if(value == -1) {
+                break;
+            }
+            marks.add(value);
+        }
+        int min = marks.get(0);
+
+        for (int i = 0; i < marks.size(); i++) {
+            int element = marks.get(i);
+            if (element<min) {
+                min = element;
+            }
+        }
+        System.out.println(min);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] array = new int[10]; // array
+        array [0] = 1;
+
+        List<Integer> list = new ArrayList<>(); // лист из целых чисел, который называется лист
+        list.add (1);
+    }
+}
